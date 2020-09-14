3e0e33191f8b22bea1c3f74bdb3a4d0b26e040ed
VERDICT <65512475+notverdict@users.noreply.github.com>
2020-09-13 20:34:24 -0500

    made cleaner

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 from PIL import Image
-import os
 
-def run(input):
-    defaultamount = 28
-    try:
-        im = Image.open(input)
-        imgwidth, imgheight = im.size
-        height = imgheight // 3
-        width = imgwidth // 9
-        for i in range(0, 3):
-            for j in range(0, 9):
-                box = (j * width, i * height, (j + 1) * width, (i + 1) * height)
-                a = im.crop(box)
-                defaultamount = defaultamount - 1
-                img = a
+class NameMC(object):
+    def __init__(self, filename):
+        self.im = Image.open(filename)
+        self.currentfilename = 28
+
+    def run(self):
+        _width, _height = self.im.size
+        height = _height // 3
+        width = _width // 9
+        for y in range(0, 3):
+            for x in range(0, 9):
+                box = (x * width, y * height, (x + 1) * width, (y + 1) * height)
+                a = self.im.crop(box)
                 background = Image.open("head.png")
-                background.paste(img, (8, 8), img)
-                background.save(f'{str(defaultamount)}.png', "PNG")
-        print('[!] Done.')
-        os.system('pause >> NUL')
-    except:
-        print('[-] File not Found.')
-        os.system('pause >> NUL')
+                background.paste(a, (8, 8), a)
+                self.currentfilename = self.currentfilename - 1
+                background.save(f'{str(self.currentfilename)}.png', "PNG")
+        input('Done.')
 
-png = input('[!] Image File Name: ')
-run(png)
+if __name__ == "__main__":
+    filename = input('Filename: ')
+    Run = NameMC(filename)
+    Run.run()
